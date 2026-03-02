37953df68b48b93465fb9d8748c7925f2ef92054
akanek0001 <112048605+akanek0001@users.noreply.github.com>
2026-03-03 00:52:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta 
 import requests
 import json
 import re
